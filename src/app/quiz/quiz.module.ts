e834eda4b09b9acdd2cad84b3afd9542dda1750e
findmunish <findmunish@gmail.com>
2020-10-05 13:10:07 +0530

    quiz component and module reorganized

--- src/app/quiz/quiz.module.ts
+++ src/app/quiz/quiz.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { QuizComponent } from './quiz/quiz.component';
-import { QuestionaireComponent } from './questionaire/questionaire.component';
+import { QuestionaireComponent } from './quiz/questionaire/questionaire.component';
 
 const routes: Routes = [
   { path: '', component: QuizComponent}

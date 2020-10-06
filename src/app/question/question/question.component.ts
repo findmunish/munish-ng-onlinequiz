dcf1259438641b10d8076be54e8245886a50fb91
findmunish <findmunish@gmail.com>
2020-10-06 23:28:13 +0530

    2 files modified

--- src/app/question/question/question.component.ts
+++ src/app/question/question/question.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router/*, ActivatedRoute, ParamMap*/ } from '@angular/router';
 
-import { ErrorsComponent } from '../../errors/errors/errors.component';
+//import { ErrorsComponent } from '../../errors/errors/errors.component';
 import { LoginService } from '../../services/login.service';
 import { QuestionBankService } from '../../services/question-bank.service';
 import { RoutingService } from '../../services/routing.service';
@@ -14,14 +14,14 @@ import { RoutingService } from '../../services/routing.service';
 export class QuestionComponent implements OnInit {
   currQuesIndex = -1;
   currQuestion: any = [];
-  checkErrors = {login:true, question:true, evaluate:true};
-  constructor(private activatedRoute: ActivatedRoute,
+  checkErrors = {login:true, question:true, evaluate:false};
+  constructor(//private activatedRoute: ActivatedRoute,
               private router:Router,
               public loginService: LoginService,
               public questionBankService: QuestionBankService,
               public routingService: RoutingService) {}
   ngOnInit(): void {
-    this.setCurrentQuestionNum(this.activatedRoute.snapshot.paramMap.get('id') );
+    //this.setCurrentQuestionNum(this.activatedRoute.snapshot.paramMap.get('id') );
     if(!this.questionBankService.doQuestionsExist()) {
       this.currQuestion = this.questionBankService.questionBank.questions[this.currQuesIndex];
     }
--- src/app/quiz/quiz/quiz.component.ts
+++ src/app/quiz/quiz/quiz.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-import { ErrorsComponent } from '../../errors/errors/errors.component';
+//import { ErrorsComponent } from '../../errors/errors/errors.component';
 import { LoginService } from '../../services/login.service';
 import { QuestionBankService } from '../../services/question-bank.service';
 import { RoutingService } from '../../services/routing.service';
@@ -13,7 +13,7 @@ import { RoutingService } from '../../services/routing.service';
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
-    checkErrors = {login:true, question:true, evaluate:false};
+    checkErrors = {login:true, question:true, evaluate:true};
   questionaireJsonLink = 'https://api.jsonbin.io/b/5f780373302a837e95736e9a';
   constructor(private httpQuestionaireClient: HttpClient,
               public loginService: LoginService,

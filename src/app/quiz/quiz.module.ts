fb483c01789645bdc4d34469e82eda9ab07f6ddb
findmunish <findmunish@gmail.com>
2020-10-06 19:43:55 +0530

    ErrorsComponent included where ever required

--- src/app/quiz/quiz.module.ts
+++ src/app/quiz/quiz.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-//import { ErrorsComponent } from '../errors/errors.component';
+import { ErrorsComponent } from '../errors/errors/errors.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { QuestionaireComponent } from './quiz/questionaire/questionaire.component';
 
@@ -15,8 +15,10 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    ErrorsComponent,
     RouterModule.forChild(routes)
   ],
   declarations: [QuizComponent, QuestionaireComponent]
 })
+
 export class QuizModule { }
